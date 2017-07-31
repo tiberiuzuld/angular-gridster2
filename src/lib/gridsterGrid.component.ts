28cbc5a2cb8cc132cc06f38c41c225f2b4188038
Tiberiu Zuld <tiberiuzuld@gmail.com>
2017-07-31 11:09:23 +0300

    fix Getting ViewDestroyedError in version 3.8.1 #93

--- src/lib/gridsterGrid.component.ts
+++ src/lib/gridsterGrid.component.ts
@@ -1,4 +1,7 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, Host, Renderer2} from '@angular/core';
+import {
+  ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, Host, OnDestroy,
+  Renderer2
+} from '@angular/core';
 import {GridsterComponent} from './gridster.component';
 
 @Component({
@@ -8,7 +11,7 @@ import {GridsterComponent} from './gridster.component';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class GridsterGridComponent {
+export class GridsterGridComponent implements OnDestroy {
   el: any;
   gridster: GridsterComponent;
   columns: Array<any>;
@@ -29,6 +32,10 @@ export class GridsterGridComponent {
     this.width = 0;
     this.columnsHeight = 0;
     this.rowsWidth = 0;
+  }
+
+  ngOnDestroy() {
+    this.cdRef.detach();
   }
 
   updateGrid(): void {
