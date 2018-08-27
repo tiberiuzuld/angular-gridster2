7ac01a5eedfd4073e2a7c8ea33555acaeb1ebf8e
CollinGraf314 <cgraf@uvm.edu>
2018-08-27 16:51:36 -0400

    fixed iframe issue for resizing elements

--- projects/angular-gridster2/src/lib/gridsterResizable.service.ts
+++ projects/angular-gridster2/src/lib/gridsterResizable.service.ts
@@ -24,6 +24,7 @@ export class GridsterResizable {
   resizeEnabled: boolean;
   mousemove: Function;
   mouseup: Function;
+  mouseleave: Function;
   cancelOnBlur: Function;
   touchmove: Function;
   touchend: Function;
@@ -88,6 +89,7 @@ export class GridsterResizable {
       this.touchmove = this.gridster.renderer.listen(this.gridster.el, 'touchmove', this.dragFunction);
     });
     this.mouseup = this.gridsterItem.renderer.listen('document', 'mouseup', this.dragStopFunction);
+    this.mouseleave = this.gridsterItem.renderer.listen('document', 'mouseleave', this.dragStopFunction);
     this.cancelOnBlur = this.gridsterItem.renderer.listen('window', 'blur', this.dragStopFunction);
     this.touchend = this.gridsterItem.renderer.listen('document', 'touchend', this.dragStopFunction);
     this.touchcancel = this.gridsterItem.renderer.listen('document', 'touchcancel', this.dragStopFunction);
@@ -173,6 +175,7 @@ export class GridsterResizable {
     cancelScroll();
     this.mousemove();
     this.mouseup();
+    this.mouseleave();
     this.cancelOnBlur();
     this.touchmove();
     this.touchend();
@@ -366,6 +369,7 @@ export class GridsterResizable {
       cancelDrag();
     }, this.gridster.$options.resizable.delayStart);
     const cancelMouse = this.gridsterItem.renderer.listen('document', 'mouseup', cancelDrag);
+    const cancelMouseLeave = this.gridsterItem.renderer.listen('document', 'mouseleave', cancelDrag);
     const cancelOnBlur = this.gridsterItem.renderer.listen('window', 'blur', cancelDrag);
     const cancelTouchMove = this.gridsterItem.renderer.listen('document', 'touchmove', cancelMove);
     const cancelTouchEnd = this.gridsterItem.renderer.listen('document', 'touchend', cancelDrag);
@@ -382,6 +386,7 @@ export class GridsterResizable {
       clearTimeout(timeout);
       cancelOnBlur();
       cancelMouse();
+      cancelMouseLeave();
       cancelTouchMove();
       cancelTouchEnd();
       cancelTouchCancel();
