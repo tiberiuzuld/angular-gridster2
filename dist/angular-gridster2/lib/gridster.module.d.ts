16bda8d8c8b8a405edd5d7bdc3258494d5ce4402
Michał Pawłoszek <mpawloszek@n-educatio.pl>
2020-05-14 14:16:45 +0200

    Prepare for Angular 8 version

--- dist/angular-gridster2/lib/gridster.module.d.ts
+++ dist/angular-gridster2/lib/gridster.module.d.ts
@@ -1,11 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './gridster.component';
-import * as ɵngcc2 from './gridsterItem.component';
-import * as ɵngcc3 from './gridsterPreview.component';
-import * as ɵngcc4 from '@angular/common';
 export declare class GridsterModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<GridsterModule, [typeof ɵngcc1.GridsterComponent, typeof ɵngcc2.GridsterItemComponent, typeof ɵngcc3.GridsterPreviewComponent], [typeof ɵngcc4.CommonModule], [typeof ɵngcc1.GridsterComponent, typeof ɵngcc2.GridsterItemComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<GridsterModule>;
 }
-
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZ3JpZHN0ZXIubW9kdWxlLmQudHMiLCJzb3VyY2VzIjpbImdyaWRzdGVyLm1vZHVsZS5kLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUE7OztBQUNBIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlY2xhcmUgY2xhc3MgR3JpZHN0ZXJNb2R1bGUge1xufVxuIl19
