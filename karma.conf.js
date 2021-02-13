a78f13ee960c35e26ef100f49114e8c63ec79e5e
Tiberiu Zuld <tiberiuzuld@gmail.com>
2021-02-13 12:22:03 +0200

    fix karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ponyracer'),
+      dir: require('path').join(__dirname, './coverage/app'),
       subdir: '.',
       reporters: [
         {type: 'html'},
