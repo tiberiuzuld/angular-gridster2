187bd6854fe7829671b7de7c4919495c9f3fdb72
Florian P. <florian.plesker@web.de>
2025-11-22 13:25:45 +0100

    fix: remove preview on item remove (#951)

--- projects/angular-gridster2/src/lib/gridsterPreview.component.ts
+++ projects/angular-gridster2/src/lib/gridsterPreview.component.ts
@@ -1,47 +1,31 @@
 import {
   Component,
   ElementRef,
-  EventEmitter,
   Input,
-  OnDestroy,
-  OnInit,
   Renderer2,
   ViewEncapsulation
 } from '@angular/core';
-import { Subscription } from 'rxjs';
 import { GridsterItem } from './gridsterItem.interface';
 import { GridsterRenderer } from './gridsterRenderer.service';
 
 @Component({
   selector: 'gridster-preview',
   template: '',
-  styleUrls: ['./gridsterPreview.css'],
-  encapsulation: ViewEncapsulation.None,
-  standalone: true
+  styleUrl: './gridsterPreview.css',
+  encapsulation: ViewEncapsulation.None
 })
-export class GridsterPreviewComponent implements OnInit, OnDestroy {
-  @Input() previewStyle$: EventEmitter<GridsterItem | null>;
+export class GridsterPreviewComponent {
   @Input() gridRenderer: GridsterRenderer;
   private el: HTMLElement;
-  private sub: Subscription;
 
-  constructor(el: ElementRef, private renderer: Renderer2) {
+  constructor(
+    el: ElementRef,
+    private renderer: Renderer2
+  ) {
     this.el = el.nativeElement;
   }
 
-  ngOnInit(): void {
-    this.sub = this.previewStyle$.subscribe(options =>
-      this.previewStyle(options)
-    );
-  }
-
-  ngOnDestroy(): void {
-    if(this.sub) {
-      this.sub.unsubscribe();
-    }
-  }
-
-  private previewStyle(item: GridsterItem | null): void {
+  previewStyle(item: GridsterItem | null): void {
     if (item) {
       this.renderer.setStyle(this.el, 'display', 'block');
       this.gridRenderer.updateItem(this.el, item, this.renderer);
